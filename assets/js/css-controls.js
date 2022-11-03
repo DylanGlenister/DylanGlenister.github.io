90df2279b62416da95f2b2a1989198a9a308439d
SlenderFox <DylanGlenister@outlook.com>
2022-11-03 17:48:03 +1100

    Personalised

---
+++ assets/js/css-controls.js
@@ -0,0 +1,42 @@
+// A switch that enables and disables light theme
+const light = document.querySelector('html').style.getPropertyValue("--localWhite");
+const dark = document.querySelector('html').style.getPropertyValue("--localBlack");
+const button = document.querySelector('input[type="checkbox"]');
+const html = document.querySelector('html');
+const body = document.querySelector('body');
+const h1 = document.querySelectorAll('h1');
+const h2 = document.querySelectorAll('h2');
+const h3 = document.querySelectorAll('h3');
+const h4 = document.querySelectorAll('h4');
+const strong = document.querySelectorAll('strong');
+const li = document.querySelectorAll('li');
+
+button.addEventListener('click', function() {
+    if (button.value === "enabled") {
+        button.value = "disabled";
+        console.log("Light theme enabled");
+        console.log(light);
+        // Colours changes
+        html.style.backgroundColor = "rgb(255, 255, 255)";
+        body.style.color = "rgb(5, 5, 5)";
+        Array.from(h1).forEach((ele) => { ele.style.color = "rgb(5, 5, 5)"; });
+        Array.from(h2).forEach((ele) => { ele.style.color = "rgb(5, 5, 5)"; });
+        Array.from(h3).forEach((ele) => { ele.style.color = "rgb(5, 5, 5)"; });
+        Array.from(h4).forEach((ele) => { ele.style.color = "rgb(5, 5, 5)"; });
+        Array.from(strong).forEach((ele) => { ele.style.color = "rgb(5, 5, 5)"; });
+        Array.from(li).forEach((ele) => { ele.style.color = "rgb(5, 5, 5)"; });
+    } else {
+        button.value = "enabled";
+        console.log("Light theme disabled");
+        console.log(dark);
+        // Colours changes
+        html.style.backgroundColor = "rgb(12, 13, 14)";
+        body.style.color = "rgb(230, 230, 230)";
+        Array.from(h1).forEach((ele) => { ele.style.color = "rgb(230, 230, 230)" });
+        Array.from(h2).forEach((ele) => { ele.style.color = "rgb(230, 230, 230)" });
+        Array.from(h3).forEach((ele) => { ele.style.color = "rgb(230, 230, 230)" });
+        Array.from(h4).forEach((ele) => { ele.style.color = "rgb(230, 230, 230)" });
+        Array.from(strong).forEach((ele) => { ele.style.color = "rgb(230, 230, 230)"; });
+        Array.from(li).forEach((ele) => { ele.style.color = "rgb(230, 230, 230)" });
+    }
+})
